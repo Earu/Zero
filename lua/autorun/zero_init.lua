6fe8bd9b770fe15619b0842e5e45231004fc667b
Earu <earu.derg@gmail.com>
2020-10-24 19:38:47 +0200

    basic code for debugging the editor in-game

--- lua/autorun/zero_init.lua
+++ lua/autorun/zero_init.lua
@@ -1,2 +1,4 @@
 AddCSLuaFile("zero/client/vgui/editor.lua")
 AddCSLuaFile("zero/zero.lua")
+
+include("zero/zero.lua")
--- lua/zero/zero.lua
+++ lua/zero/zero.lua
@@ -1,3 +1,41 @@
-if SERVER then return end
+local Zero = _G.Zero or {}
+_G.Zero = Zero
 
-include("zero/client/vgui/editor.lua")
+local TAG = "Zero"
+
+list.Set("DesktopWindows", "Zero", {
+	title = "Zero",
+	icon = "zero/logo.png",
+	init = function()
+		if not CLIENT then return end
+		Zero:CreateEditor()
+	end
+})
+
+if SERVER then
+	resource.AddFile("materials/zero/logo.png")
+end
+
+if CLIENT then
+	include("zero/client/vgui/editor.lua")
+
+	function Zero:CreateEditor()
+		if IsValid(self.Editor) then return end
+		self.Editor = vgui.Create("ZeroEditor")
+	end
+
+	Zero.NextToggle = 0
+	function Zero:ToggleEditor()
+		if not IsValid(self.Editor) then return end
+		if CurTime() >= Zero.NextToggle then
+			self.Editor:SetVisible(not self.Editor:IsVisible())
+			Zero.NextToggle = CurTime() + 0.25
+		end
+	end
+
+	hook.Add("PreRender", TAG, function()
+		if (input.IsKeyDown(KEY_LCONTROL) or input.IsKeyDown(KEY_RCONTROL)) and input.IsKeyDown(KEY_E) then
+			Zero:ToggleEditor()
+		end
+	end)
+end
